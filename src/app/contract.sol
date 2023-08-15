00a8fee50a99a9b35ffde6712e90412216a6f3ad
UTKARSH PANDEY <92950393+Utkarsh-Pandey-GitHub@users.noreply.github.com>
2023-08-15 19:34:41 +0530

    Update contract.sol

--- src/app/contract.sol
+++ src/app/contract.sol
@@ -1,1 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
+import "hardhat/console.sol";
 
+contract App{
+    // fund, withdraw, transfer
+    mapping(address=>uint256) public balances;
+    constructor(){
+        balances[msg.sender]=msg.sender.balance;
+    }
+
+    function Fund() public payable{
+        require(balances[msg.sender]>=msg.value,"The balance is not enough in sender's account");
+        balances[msg.sender]-=msg.value;
+
+    }
+
+    function Withdraw(uint256 amount) public{
+        require(address(this).balance>=amount,"The balance is not enough in contract");
+        
+        (bool callSuccess, ) = msg.sender.call{value: amount}("");
+        require(callSuccess, "Call failed");
+
+    }
+
+    function balanceSender()public  view returns(uint256){
+        return balances[msg.sender];
+    }
+    function balanceContract()public view returns(uint256){
+        return address(this).balance;
+    }
+
+}
